5817e7f027c0ba6186afff486aee5b18aa2363fb
RoscaGeorgeMihai <mihaigeorge985@yahoo.com>
2024-12-02 12:56:28 +0200

    Project structure
    Signed-off-by: RoscaGeorgeMihai <mihaigeorge985@yahoo.com>

--- Project/Project/Properties/AssemblyInfo.cs
+++ Project/Project/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CliniciMedicale")]
+[assembly: AssemblyTitle("Project")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CliniciMedicale")]
+[assembly: AssemblyProduct("Project")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Project/Project/Properties/Settings.Designer.cs
+++ Project/Project/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CliniciMedicale.Properties
+namespace Project.Properties
 {
 
 
